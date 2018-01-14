345cadfbe5fdc5059d22d11fff9eb5910035e50d
Dimlinn <dimitrilindijer@gmail.com>
2018-01-14 17:22:50 +0100

    Added network.py for socials networks

--- social/network.py
+++ social/network.py
@@ -2,13 +2,13 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import numpy as np
 
-## create graph
+# create graph
 G = nx.random_partition_graph([10,10,10], 0.25, 0.01)
 
-## plot graph
+# plot graph
 nx.draw_networkx(G)
 plt.show()
 
-## print numpy array of social graph
-## ones means edge between nodes, otherwise zero
+# print numpy array of social graph
+# ones means edge between nodes, otherwise zero
 print(nx.to_numpy_matrix(G))
